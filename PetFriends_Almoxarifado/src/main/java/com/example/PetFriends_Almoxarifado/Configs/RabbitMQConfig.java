1809759cd290a9819c9daa37dd5d184a4d8df947
DarkDzi <eduardoassisaraujo@gmail.com>
2026-06-22 01:34:44 -0300

    teste

--- PetFriends_Almoxarifado/src/main/java/com/example/PetFriends_Almoxarifado/Configs/RabbitMQConfig.java
+++ PetFriends_Almoxarifado/src/main/java/com/example/PetFriends_Almoxarifado/Configs/RabbitMQConfig.java
@@ -3,7 +3,7 @@ package com.example.PetFriends_Almoxarifado.Configs;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.support.converter.JacksonJsonMessageConverter;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -46,7 +46,7 @@ public class RabbitMQConfig {
     // Necessário para serializar/deserializar JSON nas mensagens
     @Bean
     public MessageConverter jsonMessageConverter() {
-        return new JacksonJsonMessageConverter();
+        return new Jackson2JsonMessageConverter();
     }
 
     @Bean
